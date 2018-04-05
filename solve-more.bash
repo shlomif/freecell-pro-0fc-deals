70d2c7c961b1c16aece564ee11301d13cf8c1f56
Shlomi Fish <shlomif@shlomifish.org>
2018-04-05 19:19:52 +0300

    add more

--- solve-more.bash
+++ solve-more.bash
@@ -8,4 +8,5 @@
 
 pi-make-microsoft-freecell-board -t 1214065 | fc-solve -l lg --freecells-num 0 -sam -p -t -sel | verify-solitaire-solution --freecells-num 0
 pi-make-microsoft-freecell-board -t 5430607 | fc-solve -l lg --freecells-num 0 -sam -p -t -sel -mi 2000000 | verify-solitaire-solution --freecells-num 0
-
+# pi-make-microsoft-freecell-board -t 5666087 | fc-solve -l tfts --freecells-num 0 -sam -p -t -sel -mi 2000000 | verify-solitaire-solution --freecells-num 0
+pi-make-microsoft-freecell-board -t 7432951 | fc-solve --freecells-num 0 -sam -p -t -sel -to 01ABCDE -mi 6000000 # | verify-solitaire-solution --freecells-num 0
