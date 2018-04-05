262db0676291cd9288fcc6f7b666893fe1a59570
Shlomi Fish <shlomif@shlomifish.org>
2018-04-05 17:12:01 +0300

    Build fc-solve for run.bash.

---
+++ build.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# build.bash
+# Copyright (C) 2018 shlomif <shlomif@telaviv1.shlomifish.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+../source/Tatzer -l extra_speed3 --nfc=0 && make -j8
