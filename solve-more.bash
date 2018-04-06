b8afa71b1fde5b65a1fa9cf1f80b2eeed5313d6a
Shlomi Fish <shlomif@shlomifish.org>
2018-04-06 13:02:48 +0300

    Refactoring

--- solve-more.bash
+++ solve-more.bash
@@ -15,16 +15,21 @@ s()
     echo "Trying deal = $deal"
 }
 
+_sol()
+{
+    pi-make-microsoft-freecell-board -t "$deal" | fc-solve --freecells-num 0 -sam -p -t -sel "$@"
+}
+
 ab()
 {
     echo "Trying deal = $deal using 0AB"
-    pi-make-microsoft-freecell-board -t "$deal" | fc-solve --freecells-num 0 -sam -p -t -sel -to 0AB -sp r:tf -mi 6000000
+    _sol -to 0AB -sp r:tf -mi 6000000
 }
 
 lg()
 {
     echo "Trying deal = $deal using lg"
-    pi-make-microsoft-freecell-board -t "$deal" | fc-solve -l lg --freecells-num 0 -sam -p -t -sel -mi 2000000 | verify-solitaire-solution --freecells-num 0
+    _sol -l lg -mi 2000000 | verify-solitaire-solution --freecells-num 0
 }
 
 d()
