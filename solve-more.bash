5ec365fec0748edf4215dc7688d866d332c68ec9
Shlomi Fish <shlomif@shlomifish.org>
2018-04-08 14:09:08 +0300

    add sol

--- solve-more.bash
+++ solve-more.bash
@@ -49,5 +49,6 @@ d 7432951 ab
 d 8011868 lg
 d 351098357 lg
 d 459095946 lg
+d 513097572 ab
 # pi-make-microsoft-freecell-board -t 14994542 | fc-solve --freecells-num 0 -sam -p -t -sel -me random-dfs -to '[0AB]' -seed 9 -sp r:tf -mi 2000000 # | verify-solitaire-solution --freecells-num 0
 # pi-make-microsoft-freecell-board -t 5666087 | fc-solve -l tfts --freecells-num 0 -sam -p -t -sel -mi 2000000 | verify-solitaire-solution --freecells-num 0
