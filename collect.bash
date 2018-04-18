e4b5d111969331967a2f94192a44546a6e0bae0a
Shlomi Fish <shlomif@shlomifish.org>
2018-04-18 21:05:51 +0300

    split into segments to avoid large files.

--- collect.bash
+++ collect.bash
@@ -6,9 +6,15 @@
 # Distributed under terms of the MIT license.
 #
 
+l=0fc-log.txt
 (
     cat ~/Backup/Arcs/0fc-log.txt
     grep -El '^End' ~/progs/freecell/git/fc-solve/fc-solve/0fc-b/results/* |
         xargs cat |
         grep -vE '^(Start|End)'
-) > 0fc-log.txt
+) > "$l"
+for prefix in `seq 0 99`
+do
+    export P="$prefix"
+    perl -nalE 'BEGIN {$P=$ENV{P}; $s = $P*1e8; $e = ($P+1)*1e8 - 1; } say if $F[1] >= $s && $F[1] <= $e' < "$l" > "$(printf "0fc-logs/%02d.log.txt" "$prefix")"
+done
