50c89e4ad7f7d1a3be2619bfc40b61e106144e46
Shlomi Fish <shlomif@shlomifish.org>
2018-04-18 22:22:32 +0300

    avoid temp file

--- collect.bash
+++ collect.bash
@@ -6,13 +6,11 @@
 # Distributed under terms of the MIT license.
 #
 
-l=0fc-log--pre.txt
 (
     cat ~/Backup/Arcs/0fc-log.txt
     grep -El '^End' ~/progs/freecell/git/fc-solve/fc-solve/0fc-b/results/* |
         xargs cat |
         grep -vE '^(Start|End)'
-) > "$l"
-perl -nalE 'BEGIN { sub f {$s = $P*1e8; $e = ($P+1)*1e8 - 1; $fn=(sprintf "0fc-logs/%02d.log.txt", $P); warn $fn; open $o, ">", $fn; } $P=0;f; } if ($F[1] >= $s && $F[1] <= $e) { $o->say($_); } else { ++$P;f;redo;} ; END { close$o;}' < "$l"
+) | \
+perl -nalE 'BEGIN { sub f {$s = $P*1e8; $e = ($P+1)*1e8 - 1; $fn=(sprintf "0fc-logs/%02d.log.txt", $P); warn $fn; open $o, ">", $fn; } $P=0;f; } if ($F[1] >= $s && $F[1] <= $e) { $o->say($_); } else { ++$P;f;redo;} ; END { close$o;}'
 cat 0fc-logs/*.log.txt > 0fc-log.txt
-rm -f "$l"
