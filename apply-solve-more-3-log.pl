54e57e0513b6ae7620208d7c834de838b5f0c8e4
Shlomi Fish <shlomif@shlomifish.org>
2018-06-29 21:15:42 +0300

    Add more results using the solve-more-3.bash log.
    Based on the run by the solve-more-3.bash program and the 0ab preset/scan.
    
    Find 0fc freecell deals in the freecell pro range :
    
    http://fc-solve.shlomifish.org/faq.html#what_are_ms_deals
    
    It is processed using << perl -p -i apply-solve-more-3-log.pl 0fc-logs/*.txt >>

---
+++ apply-solve-more-3-log.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+#
+# Short description for apply-lg-log.pl
+#
+# Author Shlomi Fish <shlomif@cpan.org>
+use strict;
+use warnings;
+use Path::Tiny qw/ path /;
+
+use vars qw/ %sol %imp /;
+
+BEGIN
+{
+    my $buf = path("solve-more-3-log.txt")->slurp_utf8;
+    $buf =~ s#Trying deal = ([0-9]+) using 0AB\n\n?I could not solve this game.#
+        $imp{$1} = 1;
+        #egms;
+    $buf =~ s#Trying deal = ([0-9]+) using 0AB\n\n?This game is solveable.#
+        $sol{$1} = 1;
+        #egms;
+}
+s/Int/S/ if /\AInt\t([0-9]+)\n\z/ && exists $sol{$1};
+$_='' if /\AInt\t([0-9]+)\n\z/ && exists $imp{$1};
