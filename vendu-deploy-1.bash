186bfe255fab7a683a22c0c5cd9e90dc4982d675
Shlomi Fish <shlomif@shlomifish.org>
2018-07-03 14:52:18 +0300

    Add the bash prog for vendu to deploy.
    It is not finished yet.

---
+++ vendu-deploy-1.bash
@@ -0,0 +1,44 @@
+#! /bin/bash
+#
+# vendu-deploy-1.bash
+# Copyright (C) 2018 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+set -e
+set -x
+
+base="`pwd`"
+shlomif_set="$base/shlomif-computer-settings"
+fcs_repo="$base/fc-solve"
+
+if ! test -e "$shlomif_set"
+then
+    git clone https://github.com/shlomif/shlomif-computer-settings
+fi
+
+PATH="$PATH:$shlomif_set/shlomif-settings/home-bin-executables/bin"
+
+if ! test -e "$fcs_repo"
+then
+    git clone https://github.com/shlomif/fc-solve
+fi
+
+fcs_src="$fcs_repo/fc-solve/source"
+fcs_b="$base/build"
+fcs_i="$base/fcs-inst"
+
+mkdir -p "$fcs_b"
+
+if ! test -e "$fcs_i"
+then
+    cd "$fcs_b"
+    "$fcs_src/Tatzer" -l zerofc --prefix="$fcs_i"
+    make
+    make install
+fi
+
+PATH="$PATH:$fcs_i/bin"
+
+
