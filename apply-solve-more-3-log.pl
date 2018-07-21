2d872f8418d2efa8a133d014f1244d31be09fc4c
Shlomi Fish <shlomif@shlomifish.org>
2018-07-21 22:01:39 +0300

    Finished adding to the log.
    Based on the run by this script:
    
    https://github.com/shlomif/fc-solve/blob/master/fc-solve/scripts/0fcpar.bash
    
    Find 0fc freecell deals in the freecell pro range :
    
    http://fc-solve.shlomifish.org/faq.html#what_are_ms_deals
    
    We reached the maximal ( (2 ** 33) - 1 ) seed.

--- apply-solve-more-3-log.pl
+++ apply-solve-more-3-log.pl
@@ -16,6 +16,36 @@ BEGIN
     $buf =~ s#Trying deal = ([0-9]+) using 0AB\n\n?This game is solveable.#
         $sol{$1} = 1;
         #egms;
+    my @lines = split /^/ms, $buf;
+
+LIN:
+    for ( my $i = 0 ; $i < @lines ; ++$i )
+    {
+        my $l = $lines[$i];
+        if ( my ($deal) = $l =~ /\ATrying deal = ([0-9]+) using dbm\n?\z/ )
+        {
+            my $text = $l;
+            ++$i;
+        INNER:
+            while ( $i < @lines )
+            {
+                $text .= ( $l = $lines[$i] );
+                if ( $l =~ /\ATrying deal = [0-9]+\n?\z/ )
+                {
+                    --$i;
+                    if ( $text =~ /^Could not solve successfully\.$/ms )
+                    {
+                        $imp{$deal} = 1;
+                    }
+                    last INNER;
+                }
+            }
+            continue
+            {
+                ++$i;
+            }
+        }
+    }
 }
 if ( my ($n) = /\AInt\t([0-9]+)\n\z/ )
 {
