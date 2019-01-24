12e01bec2a9f7a76baf078c3875882e0e3f6e319
Shlomi Fish <shlomif@shlomifish.org>
2019-01-25 00:37:00 +0200

    new vendu deploy code.

--- solve-more-3.bash
+++ solve-more-3.bash
@@ -12,12 +12,16 @@
 # d 96924 ab
 # d 99673 ab
 # d 100260 ab
-out='solve-more-3-log.txt'
+out='solve-more-4-log.txt'
 export START="$(tail-extract '^Trying deal = ([0-9]+)' "$out")"
+if test "$START" -lt 1000000000
+then
+    START=1000000000
+fi
 < 0fc-log.txt perl -lnE 'say $1 if /\AInt\t([0-9]+)\z/ && $1 >= $ENV{START}' | \
     (while read deal
     do
-        # d "$deal" ab_tail
-        d "$deal" dbm_tail
+        d "$deal" ab_tail
+        # d "$deal" dbm_tail
     done) 2>&1 | \
     tee -a "$out"
