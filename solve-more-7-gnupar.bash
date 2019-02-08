2654d12c1c7f394a865483ad8de52c7b5f3a0626
Shlomi Fish <shlomif@shlomifish.org>
2019-02-08 20:21:11 +0200

    Add more results using the solve-more-7.bash log.
    Based on the run by the solve-more-7.bash program and the 0AB scan/
    
    Find 0fc freecell deals in the freecell pro range :
    
    http://fc-solve.shlomifish.org/faq.html#what_are_ms_deals
    
    It is processed using << perl apply-solve-more-3-log.pl >>

---
+++ solve-more-7-gnupar.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# solve-more.bash
+# Copyright (C) 2018 shlomif <shlomif@cpan.org>
+#
+# Distributed under the terms of the MIT license.
+#
+export MAX_ITERS="${MAX_ITERS:-3000000}"
+
+. solve_common.bash
+
+# d 58844 ab
+# d 96924 ab
+# d 99673 ab
+# d 100260 ab
+out='solve-more-7-log.txt'
+export START="$(tail -1000 "$out" | grep -E '^Trying deal =' | tail-extract '^Trying deal = ([0-9]+)' -)"
+< 0fc-log.txt perl -lnE 'say $1 if /\AInt\t([0-9]+)\z/ && $1 > $ENV{START}' | \
+    parallel --group -j2 -k bash run-job-2.bash 2>&1 | \
+    tee -a "$out"
