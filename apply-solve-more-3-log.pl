1b689e14f17d1887e26c54b9eae1ebeaf2b34a67
Shlomi Fish <shlomif@shlomifish.org>
2019-03-28 09:45:32 +0200

    Add more results using the solve-more-6-gnupar.bash log.
    Based on the run by the solve-more-6-gnupar.bash program and the dbm_tail.
    
    Find 0fc freecell deals in the freecell pro range :
    
    http://fc-solve.shlomifish.org/faq.html#what_are_ms_deals
    
    It is processed using << perl -p -i apply-solve-more-3-log.pl 0fc-logs/*.txt >>

--- apply-solve-more-3-log.pl
+++ apply-solve-more-3-log.pl
@@ -14,11 +14,9 @@ sub _key
 }
 
 # "solve-more-7-log.txt", "solve-more-7--4e9--log.txt",
-# "solve-more-3-log.txt",
-foreach my $fn (
-    "solve-more-4-log.txt",       "solve-more-7--6e9--log.txt",
-    "solve-more-7--7e9--log.txt", "solve-more-7--8e9--log.txt",
-    )
+# "solve-more-7--6e9--log.txt",
+# "solve-more-7--7e9--log.txt", "solve-more-7--8e9--log.txt",
+foreach my $fn ( "solve-more-3-log.txt", "solve-more-4-log.txt", )
 {
     my $buf = path($fn)->slurp_utf8;
     $buf =~ s#Trying deal = ([0-9]+) using 0AB\n\n?I could not solve this game.#
