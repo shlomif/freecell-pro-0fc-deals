c61c262492514419501b876aedf7d39b6c3cfae5
Shlomi Fish <shlomif@shlomifish.org>
2019-07-02 18:06:39 +0300

    Add more results using the solve-more-6-gnupar.bash log.
    Based on the run by the solve-more-6-gnupar.bash program and the dbm_tail.
    
    Find 0fc freecell deals in the freecell pro range :
    
    http://fc-solve.shlomifish.org/faq.html#what_are_ms_deals
    
    It is processed using << perl -p -i apply-solve-more-3-log.pl 0fc-logs/*.txt >>

--- solve-more-6-gnupar.bash
+++ solve-more-6-gnupar.bash
@@ -15,5 +15,5 @@
 out='solve-more-3-log.txt'
 export START="$(tail -1000 "$out" | grep -E '^Trying deal =' | tail-extract '^Trying deal = ([0-9]+)' -)"
 < 0fc-log.txt perl -lnE 'say $1 if /\AInt\t([0-9]+)\z/ && $1 > $ENV{START}' | \
-    parallel --group -j2 -k bash run-job-1.bash 2>&1 | \
+    parallel --group -j3 -k bash run-job-1.bash 2>&1 | \
     tee -a "$out"
