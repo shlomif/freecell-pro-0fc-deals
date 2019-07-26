e42c587f8fd5ca5d581071e69658ae237bd6c5ad
Shlomi Fish <shlomif@shlomifish.org>
2019-07-26 21:07:24 +0300

    Add more results using the solve-more-6-gnupar.bash log.
    Based on the run by the solve-more-6-gnupar.bash program and the 0AB
    scan with 3e6 max iters.
    
    Find 0fc freecell deals in the freecell pro range :
    
    http://fc-solve.shlomifish.org/faq.html#what_are_ms_deals
    
    It is processed using << perl apply-solve-more-3-log.pl >>

--- solve-more-6-gnupar.bash
+++ solve-more-6-gnupar.bash
@@ -22,11 +22,12 @@ f1()
     tee -a "$out"
 }
 # f1
+    # perl -E 'while(my $l=<>){chomp$l;system("summary-fc-solve", $l, qw(-- --freecells-num 0 -to 0AB -sp r:tf -mi),$ENV{MAX_ITERS});}' 2>&1 | \
 f2()
 {
 export MAX_ITERS=6000000
 < 0fc-log.txt perl -lnE 'say $1 if /\AInt\t([0-9]+)\z/ && $1 > $ENV{START}' | \
-    perl -E 'while(my $l=<>){chomp$l;system("summary-fc-solve", $l, qw(-- --freecells-num 0 -to 0AB -sp r:tf -mi),$ENV{MAX_ITERS});}' 2>&1 | \
+    ( while read l; do summary-fc-solve "$l" -- --freecells-num 0 -to 0AB -sp r:tf -mi "$MAX_ITERS"; done ) 2>&1 | \
     tee -a "$out"
 }
 f2
