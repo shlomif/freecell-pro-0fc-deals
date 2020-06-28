1ca804f2c03057cc3a7391b6548b01796014c00d
Shlomi Fish <shlomif@shlomifish.org>
2020-06-28 21:20:09 +0300

    Reduce the dflt number of jobs due to sys RAM.
    I kept setting it manually.

--- solve-more-6-gnupar.bash
+++ solve-more-6-gnupar.bash
@@ -31,7 +31,7 @@ log_sink()
 f1()
 {
     gen_deals |
-    parallel --group -j"${NUM_CPUS:-4}" -k bash run-job-3.bash 2>&1 | \
+    parallel --group -j"${NUM_CPUS:-2}" -k bash run-job-3.bash 2>&1 | \
     log_sink
 }
     # perl -E 'while(my $l=<>){chomp$l;system("summary-fc-solve", $l, qw(-- --freecells-num 0 -to 0AB -sp r:tf -mi),$ENV{MAX_ITERS});}' 2>&1 | \
